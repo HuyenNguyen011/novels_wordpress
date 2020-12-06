66cf14002a38c27747fee2315f07803dd8e92ac6
Nguyễn Thị Ngọc Huyền <ntnh5600@gmail.com>
2020-12-06 18:21:26 +0700

    fix again, sql ver 8

--- wp-content/themes/basicbasic/blocks/block-add-email.php
+++ wp-content/themes/basicbasic/blocks/block-add-email.php
@@ -1,7 +1,10 @@
-<div class="add-action" style="background: #F1F1F1; margin: 10px; padding: 20px; 
+<div class="add-action" 
+    style="background: #F1F1F1; margin: 10px; padding: 20px; 
     box-shadow: 5px 10px 8px #888888; border-radius: 5px; text-align: center;">
-    <h4><?php block_field('title');?></h4>
-    <p><?php block_field('description');?></p>
+    <h4><?php block_field('username');?></h4>
+    <img src="<?php block_field('image');?>" alt="img"
+    style="border-radius: 40px; width: 80px; height: 80px;">
+    <p><?php block_field('email');?></p>
     <a style="display: inline-block; background: #7A7A7A; 
     color: #fff; padding: 12px 18px; border-radius: 5px; text-decoration: none;" 
     href="<?php block_field('button-url');?>"><?php block_field('button-text');?></a>
